4eb2493551611b801e255f92e106ec22278a0b1f
sirkibsirkib <christopher.esterhuyse@gmail.com>
2019-04-13 17:10:32 +0200

    vtables woooo

--- src/locking.rs
+++ src/locking.rs
@@ -1,17 +1,23 @@
 
-use std::any::Any;
+// use std::any::Any;
 use std::marker::PhantomData;
 use crossbeam::sync::ShardedLock;
 use std::sync::Arc;
 use std::borrow::Borrow;
 
+// unsafe impl Send for Box<Trait + 'static> {}
 trait Trait {
 	fn say(&self);
 }
+// unsafe impl Send for (dyn Trait + 'static) {}
 
 
 // trait AnyTrait: Any + Trait {}
 
+unsafe impl Send for Reader {}
+unsafe impl Sync for Reader {}
+unsafe impl<T: Trait> Send for Writer<T> {}
+unsafe impl<T: Trait> Sync for Writer<T> {}
 struct Reader {
 	x: Arc<dyn Trait>,
 }
@@ -21,8 +27,8 @@ struct Writer<T: Trait> {
 	phantom: PhantomData<*const T>,
 }
 impl<T: 'static +  Trait> Writer<T> {
-	pub fn new(innermost: T) -> Self {
-		let inner: ShardedLock<Box<dyn Trait>> = ShardedLock::new(Box::new(innermost));
+	pub fn new(innermost: Box<T>) -> Self {
+		let inner: ShardedLock<Box<dyn Trait>> = ShardedLock::new(innermost);
 		let outer: Arc<dyn Trait> = Arc::new(inner);
 		Writer {
 			x: outer,
@@ -32,43 +38,29 @@ impl<T: 'static +  Trait> Writer<T> {
 	}
 
 	pub fn inner(&self) -> &Arc<dyn Trait> {
-		// &self.x
-		unimplemented!()
+		&self.x
+		// unimplemented!()
 	}
 
-	pub fn alter<Q: 'static +  Trait>(self: Self, q: Q) -> Writer<Q> {
-
+	pub fn alter<F,Q: 'static +  Trait>(self: Self, swap_fn: F) -> Writer<Q>
+	where F: Fn(Box<T>) -> Box<Q> {
+		println!("HOTSWAP");
 		unsafe {
 			let y = self.x.borrow();
 			let x = &*(y as *const dyn Trait as *const ShardedLock<Box<dyn Trait>>);
-			let mut z = x.write().expect("HUAEY");
-			let new: Box<dyn Trait> = Box::new(q);
-			z.say();
-			let prev = std::mem::replace(&mut *z, new);
-			// *z = new;
-			z.say();
-			// x == ();
+			let mut z = x.write().expect("WRITER POISONED");
+			let trait_obj_here: &mut Box<dyn Trait> = &mut z;
+			let out: Box<dyn Trait> = std::mem::replace(trait_obj_here, std::mem::uninitialized());
+			let (boxt, _vtbl): (Box<T>, *const ()) = std::mem::transmute(out);
+
+			let boxq = swap_fn(boxt);
+			let new_trait_obj: Box<dyn Trait> = boxq;
+			// z == ();
+			// let new: Box<dyn Trait> = Box::new(q);
+			let uninit = std::mem::replace(trait_obj_here, new_trait_obj);
+			std::mem::forget(uninit);
+			std::mem::transmute(self)
 		}
-
-		unimplemented!()
-		// let t = Any::downcast_ref::<ShardedLock<Box<dyn Trait>>>;
-		// t == ();
-		// l
-		// let y = self.x.borrow();
-		// if let Some(x) = Any::downcast_ref::<ShardedLock<Box<dyn Trait>>>(y) {
-		// if let Some(x) = self.x.downcast_ref::<ShardedLock<Box<dyn Trait>>>() { // WORKS??
-		// 	let mut locked = x.write().expect("POIS");
-		// 	let mut new: Box<dyn Trait> = Box::new(q);
-		// 	let old: &mut Box<dyn Trait> = &mut locked;
-		// 	let _prev = std::mem::swap(old, &mut new);
-		// 	//prev dropped
-
-		// } else {
-		// 	panic!("DIDNT WORK")
-		// }
-		// unsafe {
-		// 	std::mem::transmute(self)
-		// }
 	}
 }
 
@@ -76,8 +68,7 @@ impl Trait for ShardedLock<Box<dyn Trait>> {
 	fn say(&self) {
 		self.read().unwrap().say()
 	}
-} 
-
+}
 struct A;
 impl Trait for A {
 	fn say(&self) {
@@ -93,9 +84,19 @@ impl Trait for B {
 
 #[test]
 fn foo() {
-	let w = Writer::new(A);
+	let w = Writer::new(Box::new(A));
+	let r = Reader{x: w.inner().clone()};
 
-	w.alter(B);
+	crossbeam::scope(|s| {
+		s.spawn(move |_| for _ in 0..100 {
+			r.x.say();
+			std::thread::sleep(std::time::Duration::from_millis(100));
+		});
+		s.spawn(move |_| {
+			std::thread::sleep(std::time::Duration::from_millis(3000));
+			w.alter(|_| Box::new(B));
+		});
+	}).unwrap();
 }
 
 impl<T: Trait> Trait for ShardedLock<Box<T>> {
