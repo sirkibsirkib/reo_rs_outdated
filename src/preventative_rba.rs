431a24a90c2a9262843862b41d2ac34dd8c4c232
sirkibsirkib <christopher.esterhuyse@gmail.com>
2019-04-21 14:13:45 +0200

    fiddling

--- src/preventative_rba.rs
+++ src/preventative_rba.rs
@@ -13,10 +13,10 @@ trait Var: Sized {}
 pub struct Tru; impl Var for Tru {}
 pub struct Fal; impl Var for Fal {}
 
-pub struct Payment<N, Z> {
+pub struct Coupon<N, Z> {
     phantom: PhantomData<(N, Z)>
 }
-impl<N,Z> Payment<N,Z> {
+impl<N,Z> Coupon<N,Z> {
     pub(crate) fn fresh() -> Self {
         Self { phantom: PhantomData::default() }
     }
@@ -45,8 +45,8 @@ impl<N,T> Porty<N,T> {
     pub(crate) fn fresh() -> Self {
         Self { phantom: PhantomData::default() }
     }
-    pub fn put<K1,K2>(&mut self, payment: Payment<N, Mem2<K1,K2>>, datum: T) -> Mem2<K1,K2> {
-        let _ = payment;
+    pub fn put<K1,K2>(&mut self, coupon: Coupon<N, Mem2<K1,K2>>, datum: T) -> Mem2<K1,K2> {
+        let _ = coupon;
         let _ = datum;
         Mem2::fresh()
     }
@@ -57,33 +57,36 @@ impl<N,T> Porty<N,T> {
 
 
 enum OptsVuu {
-    P0Vff(Payment<N0, Mem2<Fal, Fal>>),
+    P0Vff(Coupon<N0, Mem2<Fal, Fal>>),
 }
 impl OptsVuu {
     fn advance<F,R,K1,K2>(req: Mem2<K1, K2>, handler: F)
     -> R where F: FnOnce(Self)->R {
         let _ = req;
-        let decision = if std::mem::size_of::<OptsVuu>() == 0 {
-            unsafe {std::mem::uninitialized()}
-        } else {
-            unreachable!()
-        };
+        let decision = OptsVuu::P0Vff(Coupon::fresh());
         handler(decision)
     }
 }
 
 
-enum OptsVuf {
-    P0Vtu(Payment<N0, Mem2<Tru, Unk>>),
+enum OptsVxf<K1> {
+    P0Vtu(Coupon<N0, Mem2<Tru, Unk>>),
+    P0Vxf(Coupon<N0, Mem2<K1, Fal>>),
 }
-impl OptsVuf {
-    fn advance<F,R,V1>(req: Mem2<V1, Fal>, handler: F)
+impl<K1> OptsVxf<K1> {
+    fn advance<F,R>(req: Mem2<K1, Fal>, handler: F)
     -> R where F: FnOnce(Self)->R {
         let _ = req;
-        let decision = if std::mem::size_of::<OptsVuf>() == 0 {
-            unsafe {std::mem::uninitialized()}
-        } else {
+        let decision = if std::mem::size_of::<Self>() == 0 {
+            // unsafe {std::mem::uninitialized()}
             unreachable!()
+        } else {
+        	// some bogus runtime check
+        	if unsafe {std::mem::transmute::<_,usize>(&req as *const Mem2<K1,Fal>)} & 0b1000 == 0 {
+	        	OptsVxf::P0Vtu(Coupon::fresh())
+	        } else {
+	        	OptsVxf::P0Vxf(Coupon::fresh())
+	        }
         };
         handler(decision)
     }
@@ -98,10 +101,11 @@ fn atomic(start: Mem2<Tru, Unk>, mut porty: Porty<N0, u32>) -> ! {
     let mut uu = start.weaken_1();
     loop {
     	let ff = OptsVuu::advance(uu, |opts| match opts {
-            OptsVuu::P0Vff(payment) => porty.put(payment, 1),
+            OptsVuu::P0Vff(coupon) => porty.put(coupon, 1),
         });
-        uu = OptsVuf::advance(ff, |opts| match opts {
-            OptsVuf::P0Vtu(payment) => porty.put(payment, 1).weaken_1()
+        uu = OptsVxf::advance(ff, |opts| match opts {
+            OptsVxf::P0Vtu(coupon) => porty.put(coupon, 2).weaken_1(),
+            OptsVxf::P0Vxf(coupon) => porty.put(coupon, 3).weaken_1().weaken_2(),
         });
     }
 }
