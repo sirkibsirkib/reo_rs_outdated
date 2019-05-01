8eb5528cc3e8173b792890ddcfbf7c72069e6980
sirkibsirkib <christopher.esterhuyse@gmail.com>
2019-05-01 17:46:51 +0200

    i think fixed

--- src/rba_preprocess.rs
+++ src/rba_preprocess.rs
@@ -1,8 +1,27 @@
 type PortId = u32;
-const LEN: usize = 4;
+const LEN: usize = 3;
 use std::{fmt, mem};
 
-#[derive(Debug, Copy, Clone, Eq, PartialEq)]
+use hashbrown::HashSet;
+
+macro_rules! hashset {
+    (@single $($x:tt)*) => (());
+    (@count $($rest:expr),*) => (<[()]>::len(&[$(hashset!(@single $rest)),*]));
+
+    ($($key:expr,)+) => { hashset!($($key),+) };
+    ($($key:expr),*) => {
+        {
+            let _cap = hashset!(@count $($key),*);
+            let mut _set = HashSet::with_capacity(_cap);
+            $(
+                let _ = _set.insert($key);
+            )*
+            _set
+        }
+    };
+}
+
+#[derive(Debug, Copy, Clone, Eq, PartialEq, Hash)]
 enum Val {
 	T, F, X,
 }
@@ -15,7 +34,8 @@ impl Val {
 	}
 	pub fn mismatches(self, other: Self) -> bool {
 		use Val::*;
-		match [self, other] {
+		let s: [Val;2] = [self, other];
+		match s {
 			[T,F] | [F,T] => true,
 			_ => false,
 		}
@@ -23,38 +43,41 @@ impl Val {
 }
 
 #[derive(Debug, Clone)]
-struct Rba {
+pub struct Rba {
 	rules: Vec<Rule>,
 }
 impl Rba {
-	pub fn normalize(&self) -> Rba {
+	pub fn normalize(mut self) -> Self {
 		let mut buf = vec![];
-		let mut rba = self.clone();
-		while let Some(idx) = rba.first_silent_idx() {
-			let silent = &rba.rules[idx];
-			println!("TODO. Remove silent rule at idx={}", idx);
-			for (i,r) in rba.rules.iter().enumerate() {
-				if i == idx {continue}
+		while let Some(idx) = self.first_silent_idx() {
+			let silent = self.rules.remove(idx);
+			println!("... Removing silent rule at idx={}", idx);
+			if silent.no_effect() {
+				// when [silent . x] == x
+				continue;
+			}
+			for (i,r) in self.rules.iter().enumerate() {
 				if let Some(composed) = silent.compose(r) {
-					println!("ADDING rule ({},{})", i, idx);
+					println!("ADDING rule ({},{})", idx, i);
 					buf.push(composed);
 				}
 			}
-			let _ = rba.rules.remove(idx);
-			rba.rules.append(&mut buf);
-			println!("AFTER: {:#?}\n----------------", &rba.rules);
+			self.rules.append(&mut buf);
+			println!("AFTER: {:#?}\n----------------", &self.rules);
+			self = self.rule_merge();
+			println!("... rules_merged {:#?}", &self.rules);
 		}
-		rba.minimize()
+		self
 	}
 	pub fn first_silent_idx(&self) -> Option<usize> {
 		self.rules.iter().enumerate().filter(|(_,r)| r.is_silent()).map(|(i,_)| i).next()
 	}
-	pub fn minimize(mut self) -> Self {
+	pub fn rule_merge(mut self) -> Self {
 		'outer: loop {
 			for (idx1, r1) in self.rules.iter().enumerate() {
 				let rng = (idx1 + 1)..;
 				for (r2, idx2) in self.rules[rng.clone()].iter().zip(rng) {
-					if let Some(new_rule) = r1.pair_collapse(r2) {
+					if let Some(new_rule) = r1.try_merge(r2) {
 						let _ = mem::replace(&mut self.rules[idx1], new_rule);
 						self.rules.remove(idx2);
 						continue 'outer;
@@ -67,7 +90,7 @@ impl Rba {
 }
 
 
-#[derive(Clone)]
+#[derive(Clone, Eq, PartialEq, Hash)]
 struct Rule {
 	// invariant: an X in assignment implies an X in guard at same position
 	guard: [Val; LEN],
@@ -75,7 +98,10 @@ struct Rule {
 	assign: [Val; LEN],
 }
 impl Rule {
-	pub fn pair_collapse(&self, other: &Self) -> Option<Rule> {
+	pub fn no_effect(&self) -> bool {
+		self.port.is_none() && self.guard == self.assign 
+	}
+	pub fn try_merge(&self, other: &Self) -> Option<Rule> {
 		if self.port != other.port
 		|| &self.assign != &other.assign {
 			None
@@ -102,7 +128,6 @@ impl Rule {
 		}
 		let port: Option<PortId> = self.port.or(other.port);
 		let mut guard = self.guard.clone();
-		let assign = other.assign.clone();
 		// where the LATTER rule specifies something the FORMER leaves generic, specify it.
 		// Eg: [X->X . F->T] becomes [F->T] not [X->T]
 		for (&g2, &a1, ng) in izip!(other.guard.iter(), self.assign.iter(), guard.iter_mut()) {
@@ -110,6 +135,15 @@ impl Rule {
 				*ng = g2;
 			}
 		}
+		// where the FORMER rule specifies something the LATTER leaves generic, specify it.
+		// Eg: [F->F . X->X] becomes [F->F] not [F->X]
+		let mut assign = other.assign.clone();
+		for (a, &a1, &a2) in izip!(assign.iter_mut(), self.assign.iter(), other.assign.iter()) {
+			if a1.specific() && a2.generic() {
+				*a = a1;
+			}
+		}
+
 		Some(Rule::new(guard, port, assign))
 	}
 	pub fn new(guard: [Val; LEN], port: Option<PortId>, mut assign: [Val; LEN]) -> Self {
@@ -156,20 +190,34 @@ fn testy() {
 	wahey()
 }
 
+pub fn project(mut rba: Rba, atomic_ports: HashSet<PortId>) -> Rba {
+	for rule in rba.rules.iter_mut() {
+		if let Some(p) = rule.port {
+			if !atomic_ports.contains(&p) {
+				// hide!
+				rule.port = None;
+			}
+		}
+	}
+	let rba2 = rba.normalize();
+	rba2
+}
+
 pub fn wahey() {
 	use Val::*;
 	let rba = Rba { rules: vec![
-		Rule::new([X,X,X,F], None   , [X,X,X,T]),
-		Rule::new([X,X,F,T], None   , [X,X,T,F]),
-		Rule::new([X,F,T,T], Some(2), [X,T,F,F]),
-		Rule::new([F,T,T,T], None, [T,F,F,F]),
-		Rule::new([T,T,T,T], Some(1), [F,F,F,F]),
+		Rule::new([X,X,F], Some(1), [X,X,T]),
+		Rule::new([X,F,T], Some(2), [X,T,F]),
+		Rule::new([F,T,T], Some(3), [T,F,F]),
+		Rule::new([T,T,T], Some(4), [F,F,F]),
 	]};
 	println!("BEFORE");
 	for r in rba.rules.iter() {
 		println!("{:?}", r);
 	}
-	for r in rba.normalize().rules.iter() {
-		println!("{:?}", r);
-	}
+	let atomic_ports = hashset!{2,4};
+	let start = std::time::Instant::now();
+	let rba2 = project(rba, atomic_ports);
+	println!("ELAPSED {:?}", start.elapsed());
+	println!("AFTER: {:#?}", rba2);
 }
