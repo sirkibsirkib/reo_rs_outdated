25b8b60e839758456047af874cb8bd07958d7148
sirkibsirkib <christopher.esterhuyse@gmail.com>
2019-05-15 13:06:11 +0200

    let's see if that did the trick

--- src/mem.rs
+++ src/mem.rs
@@ -4,6 +4,7 @@
 
 
 
+use std::mem::ManuallyDrop;
 use std::cell::UnsafeCell;
 use parking_lot::Mutex;
 
@@ -18,7 +19,7 @@ use std::sync::atomic::{AtomicBool, Ordering};
 use anymap::AnyMap;
 
 struct Ptr {
-	ptr: UnsafeCell<*const ()>,
+	ptr: UnsafeCell<*mut ()>,
 	owned: AtomicBool,
 }
 impl Ptr {
@@ -44,36 +45,87 @@ impl Ptr {
 	fn try_dec(&self) -> bool {
 		self.owned.swap(false, Ordering::SeqCst)
 	}
+}
+
+struct MePtr(Ptr);
+impl MePtr {
+	fn new(storage: *const u8, initialized: bool) -> Self {
+		Self(Ptr {
+			ptr: UnsafeCell::new(unsafe { std::mem::transmute(storage) }),
+			owned: initialized.into(),
+		})
+	}
+}
+struct PoPtr(Ptr);
+impl PoPtr {
 	fn stack_put<T>(&self, datum_ref: &T) {
 		unsafe {
-			*self.ptr.get() = std::mem::transmute(datum_ref);
+			*self.0.ptr.get() = std::mem::transmute(datum_ref);
 		}
-		assert_eq!(false, self.owned.swap(true, Ordering::SeqCst));
+		assert_eq!(false, self.0.owned.swap(true, Ordering::SeqCst));
 	}
-	fn new_dangling() -> Self {
+}
+
+
+struct MePuSpace {
+	ptr: MePtr,
+	getters_left: AtomicUsize, // counts down. every getter decrements by 1. last getter must clean up
+	drop_fn: fn(*mut ()), // part of cleaning up. types erased at this stage
+}
+impl MePuSpace {
+	fn new(ptr: MePtr, drop_fn: fn(*mut ())) -> Self {
 		Self {
-			ptr: UnsafeCell::new(std::ptr::null()),
-			owned: false.into(),
+			ptr,
+			getters_left: 0.into(),
+			drop_fn,
 		}
 	}
-
-	fn new_mem(at: *const u8, initialized: bool) -> Self {
-		Ptr {
-			ptr: UnsafeCell::new(unsafe { std::mem::transmute(at) }),
-			owned: initialized.into(),
+	fn getter_done(&self) {
+		let left = self.getters_left.fetch_sub(1, Ordering::SeqCst);
+		assert!(left >= 1);
+		if left == 1 {
+			// I was the last. perform cleanup
+			(self.drop_fn)(unsafe {
+				*self.ptr.0.ptr.get()
+			})
 		}
 	}
 }
 
-struct PutterSpace {
-	dups: AtomicUsize,
-	ptr: Ptr,
-	sema: Semaphore,
+struct PoPuSpace {
+	ptr: PoPtr,
+	dropbox: MsgDropbox, // used only by this guy to recv messages
+	getters_sema: Semaphore,
 }
-impl PutterSpace {
-	fn new(ptr: Ptr) -> Self {
-		Self {dups: 1.into(), ptr, sema: Semaphore::new(0)}
+impl PoPuSpace {
+	fn new() -> Self {
+		Self {
+			ptr: PoPtr(Ptr {
+				ptr: UnsafeCell::new(std::ptr::null_mut()),
+				owned: false.into(),
+			}),
+			dropbox: MsgDropbox::new(),
+			getters_sema: Semaphore::new(0),
+		}
+	} 
+}
+
+struct PoGeSpace {
+	dropbox: MsgDropbox, // used only by this guy to recv messages
+}
+impl PoGeSpace {
+	fn new() -> Self {
+		Self {
+			dropbox: MsgDropbox::new(),
+		}
 	}
+}
+
+enum SpaceRef<'a> {
+	MePu(&'a MePuSpace),
+	PoPu(&'a PoPuSpace),
+	PoGe(&'a PoGeSpace),
+	None,
 }
 
 struct ProtoW {
@@ -110,9 +162,26 @@ struct Rule {
 
 struct ProtoR {
 	mem_data: Vec<u8>,
-	spaces: Vec<PutterSpace>, // id range 0..(#MePu + #PoPu)
-	num_mems: usize, // == #MePu
-	dropboxes: Vec<MsgDropbox>, // id range #MePu..#PoPu
+	me_pu: Vec<MePuSpace>,  // id range 0..#MePu
+	po_pu: Vec<PoPuSpace>, // id range #MePu..(#MePu+#PoPu)
+ 	po_ge: Vec<PoGeSpace>,   // id range (#MePu+#PoPu)..(#MePu+#PoPu+#PoGe)
+ 	// me_ge doesn't need a space
+}
+impl ProtoR {
+	fn get_space(&self, mut id: PortId) -> SpaceRef {
+		if id < self.me_pu.len() {
+			return SpaceRef::MePu(unsafe { self.me_pu.get_unchecked(id) })
+		}
+		id -= self.me_pu.len();
+		if id < self.po_pu.len() {
+			return SpaceRef::PoPu(unsafe { self.po_pu.get_unchecked(id) })
+		}
+		id -= self.po_pu.len();
+		if id < self.po_ge.len() {
+			return SpaceRef::PoGe(unsafe { self.po_ge.get_unchecked(id) })
+		}
+		SpaceRef::None
+	}
 }
 
 struct MsgDropbox {
@@ -134,6 +203,9 @@ impl MsgDropbox {
 	}
 }
 
+
+
+// [MePu | PoPu | PoGe | MeGe]
 struct ProtoAll {
 	r: ProtoR,
 	w: Mutex<ProtoW>,
@@ -146,20 +218,39 @@ struct Getter<T> {
 }
 impl<T> Getter<T> {
 	fn get(&mut self) -> T {
-		let dropbox = &self.p.r.dropboxes[self.id - self.p.r.num_mems];
+		let po_ge = if let SpaceRef::PoGe(x) = self.p.r.get_space(self.id) {
+			x
+		} else {
+			panic!("WRONG ID")
+		};
 
 		// 1. enter, participate in protocol
 		self.p.w.lock().enter(&self.p.r, self.id);
 
 		// 3. wait for a putter
-		let putter_id = dropbox.recv();
+		let putter_id = po_ge.dropbox.recv();
 
-		// 4. get datum
-		let space = &self.p.r.spaces[putter_id];
-		let datum: T = space.ptr.get_move().expect("MOVE FAILED");
+		let datum = match self.p.r.get_space(putter_id) {
+			SpaceRef::MePu(me_pu) => {
+				// 4. get datum
+				let datum = me_pu.ptr.0.get_move().expect("MOVE FAILED");
 
-		// 5. release putter
-		space.sema.release();
+				// 5. perform cleanup if last getter
+				me_pu.getter_done();
+
+				datum
+			},
+			SpaceRef::PoPu(po_pu) => {
+				// 4. get datum
+				let datum = po_pu.ptr.0.get_move().expect("MOVE FAILED");
+
+				// 5. release port putter
+				po_pu.getters_sema.release();
+
+				datum
+			},
+			_ => panic!("bad putter!"),
+		};
 
 		// 6. return datum
 		datum
@@ -172,25 +263,28 @@ struct Putter<T> {
 }
 impl<T> Putter<T> {
 	fn put(&mut self, datum: T) -> Option<T> {
-		let space = &self.p.r.spaces[self.id];
-		let dropbox = &self.p.r.dropboxes[self.id - self.p.r.num_mems];
+		let po_pu = if let SpaceRef::PoPu(x) = self.p.r.get_space(self.id) {
+			x
+		} else {
+			panic!("WRONG ID")
+		};
 
 		// 1. make ready my datum
-		space.ptr.stack_put(&datum);
+		po_pu.ptr.stack_put(&datum);
 
 		// 2. enter, participate in protocol
 		self.p.w.lock().enter(&self.p.r, self.id);
 
 		// 3. wait for my value to be consumed
-		let num_getters = dropbox.recv();
+		let num_getters = po_pu.dropbox.recv();
 
 		// 4. wait for getters to finish with my value
 		for _ in 0..num_getters {
-			space.sema.acquire();
+			po_pu.getters_sema.acquire();
 		}
 
 		// 5. return datum if nobody moved it
-		let owned = space.ptr.try_dec();
+		let owned = po_pu.ptr.0.try_dec();
 		if owned {
 			Some(datum)
 		} else {
@@ -201,6 +295,34 @@ impl<T> Putter<T> {
 }
 
 
+
+/* ACT A "mem drain"
+1. Memory putter
+2. zero mem getters
+*/
+
+
+/* ACT B "mem swap"
+1. Memory putter x
+2. 1+ mem getters
+3. x not getting
+*/
+
+/* ACT C "mem stay"
+1. Memory putter x
+2. 1+ mem getters
+3. x is getting
+*/
+/* ACT D "port put"
+1. Port putter
+*/
+
+macro_rules! action {
+	($p:expr; @MEM_DRAIN@ $me_pu:expr => $( $po_ge:expr ),*) => {{
+
+	}};
+}
+
 trait Proto: Sized {
 	type Interface: Sized;
 	fn instantiate() -> Self::Interface;	
@@ -210,32 +332,22 @@ impl Proto for MyProto {
 
 	type Interface = (Putter<u32>, Putter<u32>, Getter<u32>);
 	fn instantiate() -> Self::Interface {
-
-		// buf and memspaces
 		let mem_infos = vec![
-			// one line per BUFFER putter
 			TypeMemInfo::new::<u32>(),
 		];
-		let (mem_data, ptrs) = build_buffer(mem_infos);
-		let num_mems = ptrs.len();
-		let mut spaces: Vec<_> = ptrs.into_iter().map(|ptr| PutterSpace::new(ptr)).collect();
+		let po_pu_rng = 1..=2;
+		let po_ge_rng = 3..=3;
 
-		let port_putter_ids = 1..=2;
-		let num_port_putters = port_putter_ids.clone().count();
-		for _ in port_putter_ids {
-			spaces.push(PutterSpace::new(Ptr::new_dangling()));
-		};
+		let (mem_data, me_pu) = build_buffer(mem_infos);
+		let po_pu = po_pu_rng.map(|_| PoPuSpace::new()).collect();
+		let po_ge = po_ge_rng.map(|_| PoGeSpace::new()).collect();
 
-		let port_getter_ids = 3..=3;
-		let num_port_getters = port_getter_ids.count();
-		let dropboxes = std::iter::repeat_with(|| MsgDropbox::new()).take(num_port_putters + num_port_getters).collect();
-
-		let r = ProtoR { mem_data, spaces, num_mems, dropboxes };
+		let r = ProtoR { mem_data, me_pu, po_pu, po_ge };
 		let rules = vec![
 			Rule {
 				guard: bitset!{0, 3},
 				actions: |_p| {
-
+					action![_p; @MEM_DRAIN@ 0 => 3];
 				},
 			},
 			Rule {
@@ -265,30 +377,39 @@ impl Proto for MyProto {
 	}
 }
 
+
+
 #[derive(Debug)]
 struct TypeMemInfo {
 	size: usize,
 	align: usize,
+	drop_fn: fn(*mut ()),
 }
 impl TypeMemInfo {
 	pub fn new<T>() -> Self {
+		let drop_fn: fn(*mut ()) = |ptr| unsafe {
+			let ptr: &mut ManuallyDrop<T> =
+				std::mem::transmute(ptr);
+			ManuallyDrop::drop(ptr);
+		};
 		Self {
 			size: std::mem::size_of::<T>(),
 			align: std::mem::align_of::<T>(),
+			drop_fn,
 		}
 	}
 }
 
-fn build_buffer<I>(infos: I) -> (Vec<u8>, Vec<Ptr>) where I: IntoIterator<Item=TypeMemInfo> {
+fn build_buffer<I>(infos: I) -> (Vec<u8>, Vec<MePuSpace>) where I: IntoIterator<Item=TypeMemInfo> {
 	let mut capacity = 0;
-	let mut offsets = vec![];
+	let mut offsets_n_drops = vec![];
 	for info in infos.into_iter() {
 		let rem = capacity % info.align;
 		if rem > 0 {
 			capacity += info.align - rem;
 		}
 		println!("@ {:?} for info {:?}", capacity, &info);
-		offsets.push(capacity);
+		offsets_n_drops.push((capacity, info.drop_fn));
 		capacity += info.size;
 	}
 	println!("CAP IS {:?}", capacity);
@@ -297,9 +418,10 @@ fn build_buffer<I>(infos: I) -> (Vec<u8>, Vec<Ptr>) where I: IntoIterator<Item=T
 	unsafe {
 		buf.set_len(capacity);
 	}
-	let ptrs = offsets.into_iter().map(|offset| unsafe {
+	let ptrs = offsets_n_drops.into_iter().map(|(offset, drop_fn)| unsafe {
 		let p = buf.as_ptr().offset(offset as isize);
-		Ptr::new_mem(p, false)
+		let me_ptr = MePtr::new(p, false);
+		MePuSpace::new(me_ptr, drop_fn)
 	}).collect();
 	(buf, ptrs)
 }
